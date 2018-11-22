f566c7992765517f1e32867096557229ceda8ed0
Aimee Gerold <aimee.gerold@gmail.com>
2018-11-21 19:07:35 -0500

    Initial commit: Settings and Notifications work, need to integrate data via Bluetoooth and timing of notifications

---
+++ app/src/main/java/burnnotice/capstone/alara/BluetoothActivity.java
@@ -0,0 +1,243 @@
+package burnnotice.capstone.alara;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.res.TypedArrayUtils;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import static java.lang.Double.NaN;
+
+// AG: Modified from https://stackoverflow.com/questions/13450406/ ...
+// ... how-to-receive-serial-data-using-android-bluetooth
+
+public class BluetoothActivity extends Activity
+{
+    TextView myLabel;
+    EditText myTextbox;
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
+
+    // AG: Initialize variables
+    double dbl_data, volts, uv_index, instant_irradiance, cumulative_irradiance, int_progress, user_MED = NaN;
+    String str_progress = "";
+    List<Double> history_irradiance = new ArrayList<>();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+        Button openButton = (Button)findViewById(R.id.open);
+        Button sendButton = (Button)findViewById(R.id.send);
+        Button closeButton = (Button)findViewById(R.id.close);
+        myLabel = (TextView)findViewById(R.id.label);
+        myTextbox = (EditText)findViewById(R.id.entry);
+
+        //Open Button
+        openButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                try
+                {
+                    findBT();
+                    openBT();
+                }
+                catch (IOException ex) { }
+            }
+        });
+
+        //Send Button
+        sendButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                try
+                {
+                    sendData();
+                }
+                catch (IOException ex) { }
+            }
+        });
+
+        //Close button
+        closeButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                try
+                {
+                    closeBT();
+                }
+                catch (IOException ex) { }
+            }
+        });
+    }
+
+    public void findBT()
+    {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null)
+        {
+            myLabel.setText("No bluetooth adapter available");
+        }
+
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice device : pairedDevices)
+            {
+                if(device.getName().equals("Burn Notice BLE1"))
+                {
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+        myLabel.setText("Bluetooth Device Found");
+    }
+
+    void openBT() throws IOException
+    {
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID
+        mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+        mmSocket.connect();
+        mmOutputStream = mmSocket.getOutputStream();
+        mmInputStream = mmSocket.getInputStream();
+
+        beginListenForData();
+
+        myLabel.setText("Bluetooth Opened");
+    }
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+
+        // TODO: Change delimiter to find data of interest
+        final byte delimiter = 10; //This is the ASCII code for a newline character
+
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+                                            myLabel.setText(data);
+                                            calculateExposure(data);
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+    }
+
+    // AG: Created method to take input data and calculate exposure
+    void calculateExposure(String str_data) {
+        dbl_data = Double.parseDouble(str_data); // AG: convert string to double
+        if (dbl_data < 0) { // AG: if negative value, assume negligible and approximately 0
+            dbl_data = 0;
+        }
+
+        // TODO: Change voltage as necessary
+        volts = dbl_data / 4096 * 5; // AG: convert bits (bytes?) to voltage
+        uv_index = volts / 10;
+        instant_irradiance = uv_index * 0.0025; // AG: convert UV index to instant_irradiance (1 = 0.0025 mW/cm^2)
+        // AG: UV index conversion according to https://escholarship.org/uc/item/5925w4hq
+        cumulative_irradiance = cumulative_irradiance + instant_irradiance;
+        // TODO: Cumulative irradiance must update progress bar (content_main.xml)
+        int_progress = cumulative_irradiance / user_MED;
+        str_progress = Double.toString(int_progress);
+        // TODO: Must factor in time prior to skin type MED/preference time
+        // TODO: Must trigger NotificationActivity
+        history_irradiance.add(instant_irradiance);
+        System.out.println(cumulative_irradiance);
+        // TODO: Remove print statement after verifying functionality
+    }
+
+    void sendData() throws IOException
+    {
+        String msg = myTextbox.getText().toString();
+        msg += "\n";
+        mmOutputStream.write(msg.getBytes());
+        myLabel.setText("Data Sent");
+    }
+
+    void closeBT() throws IOException
+    {
+        stopWorker = true;
+        mmOutputStream.close();
+        mmInputStream.close();
+        mmSocket.close();
+        myLabel.setText("Bluetooth Closed");
+    }
+}
---
+++ app/src/main/java/burnnotice/capstone/alara/NotificationActivity.java
@@ -0,0 +1,201 @@
+package burnnotice.capstone.alara;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+/**
+ * Helper class for showing and canceling exposure
+ * notifications.
+ * <p>
+ * This class makes heavy use of the {@link NotificationCompat.Builder} helper
+ * class to create notifications in a backward-compatible way.
+ */
+public class NotificationActivity {
+    /**
+     * The unique identifier for this type of notification.
+     */
+    private static final String NOTIFICATION_TAG = "Exposure";
+
+    /**
+     * Shows the notification, or updates a previously shown notification of
+     * this type, with the given parameters.
+     * <p>
+     * Customize the contents of this method to tweak the behavior and
+     * presentation of exposure notifications. Make sure to follow the
+     * <a href="https://developer.android.com/design/patterns/notifications.html">
+     * Notification design guidelines</a> when doing so.
+     *
+     * @see #cancel(Context)
+     */
+    public static void notifyExposure(final Context context,
+                                      final String exampleString, final int number) {
+        final Resources res = context.getResources();
+
+        // This image is used as the notification's large icon (thumbnail).
+        final Bitmap picture = BitmapFactory.decodeResource(res, R.drawable.sun_icon);
+
+
+        final String ticker = exampleString;
+        final String title = res.getString(
+                R.string.notification_title, exampleString);
+        final String text = res.getString(
+                R.string.notification_text, exampleString);
+
+        // AG: Adding for Android 8.0 and later, need channel ID
+        int NOTIFICATION_ID = 234;
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+
+            String CHANNEL_ID = "my_channel_01";
+            CharSequence name = "my_channel";
+            String Description = "This is my channel";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.setDescription(Description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setShowBadge(false);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+
+                // Set appropriate defaults for the notification light, sound,
+                // and vibration.
+                .setDefaults(Notification.DEFAULT_ALL)
+
+                // Set required fields, including the small icon, the
+                // notification title, and text.
+
+                // AG: Cannot exclude small icon, or notification will not send
+                .setSmallIcon(R.drawable.ic_stat_exposure)
+                .setContentTitle(title)
+                .setContentText(text)
+
+                // All fields below this line are optional.
+
+                // Use a default priority (recognized on devices running Android
+                // 4.1 or later)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+                // Provide a large icon, shown with the notification in the
+                // notification drawer on devices running Android 3.0 or later.
+                .setLargeIcon(picture)
+
+                // Set ticker text (preview) information for this notification.
+                .setTicker(ticker)
+
+                // Show a number. This is useful when stacking notifications of
+                // a single type.
+                .setNumber(number)
+
+                // If this notification relates to a past or upcoming event, you
+                // should set the relevant time information using the setWhen
+                // method below. If this call is omitted, the notification's
+                // timestamp will by set to the time at which it was shown.
+
+                // Call setWhen if this notification relates to a past or
+                // upcoming event. The sole argument to this method should be
+                // the notification timestamp in milliseconds.
+                //.setWhen(...)
+
+                // Set the pending intent to be initiated when the user touches
+                // the notification.
+                .setContentIntent(
+                        PendingIntent.getActivity(
+                                context,
+                                0,
+                                new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com")),
+                                PendingIntent.FLAG_UPDATE_CURRENT))
+
+                // Show expanded text content on devices running Android 4.1 or
+                // later.
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(text)
+                        .setBigContentTitle(title)
+
+                        // AG: Comment shown at bottom of notification
+                        .setSummaryText("We recommend reapplying sunscreen or shade soon!"))
+
+                // Example additional actions for this notification. These will
+                // only show on devices running Android 4.1 or later, so you
+                // should ensure that the activity in this notification's
+                // content intent provides access to the same actions in
+                // another way.
+//                .addAction(
+//                        R.drawable.ic_action_stat_share,
+//                        res.getString(R.string.action_share),
+//                        PendingIntent.getActivity(
+//                                context,
+//                                0,
+//                                Intent.createChooser(new Intent(Intent.ACTION_SEND)
+//                                        .setType("text/plain")
+//                                        .putExtra(Intent.EXTRA_TEXT, "Dummy text"), "Dummy title"),
+//                                PendingIntent.FLAG_UPDATE_CURRENT))
+//                .addAction(
+//                        R.drawable.ic_action_stat_reply,
+//                        res.getString(R.string.action_reply),
+//                        null)
+
+                // Automatically dismiss the notification when it is touched.
+                .setAutoCancel(true);
+
+        // AG: Need to add channel stuff for Android 8.0 and later
+        Intent resultIntent = new Intent(context, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(resultPendingIntent);
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+        // AG: Removed redundant call. See above. ^
+//        notifyExposure(context, builder.build());
+    }
+
+    @TargetApi(Build.VERSION_CODES.ECLAIR)
+    private static void notifyExposure(final Context context, final Notification notification) {
+        final NotificationManager nm = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            nm.notify(NOTIFICATION_TAG, 0, notification);
+        } else {
+            nm.notify(NOTIFICATION_TAG.hashCode(), notification);
+        }
+    }
+
+    /**
+     * Cancels any notifications of this type previously shown using
+     * {@link #notifyExposure(Context, String, int)}.
+     */
+    @TargetApi(Build.VERSION_CODES.ECLAIR)
+    public static void cancel(final Context context) {
+        final NotificationManager nm = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            nm.cancel(NOTIFICATION_TAG, 0);
+        } else {
+            nm.cancel(NOTIFICATION_TAG.hashCode());
+        }
+    }
+}
